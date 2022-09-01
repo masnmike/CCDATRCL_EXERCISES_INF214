b5be005f5475695912794d0e0eb2f2d8fcfae3c7
Michael Angelo S. Natividad <111556609+masnmike@users.noreply.github.com>
2022-09-01 10:56:22 +0800

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -9,6 +9,6 @@ public class Weapon {
     // Weapon method
     public void weaponDescription() {
         System.out.println("This is the " + name + ", it hurts as much as " + damage + 
-        " damage, it's heavy as " + weight + " kilos and its rarity is " + rarity + "!");
+        " damage, it's as heavy as " + weight + " kilos and its rarity is " + rarity + "!");
     }
 }
